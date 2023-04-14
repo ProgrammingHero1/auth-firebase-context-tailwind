b83140786291a6cc80c1e297400e8deb11dbdaa1
Programming Hero <programming.hero1@gmail.com>
2023-04-14 18:53:07 -0500

    observer auth state change

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProviders';
 
 const Header = () => {
+
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.error(error))
+    }
+
     return (
         <div>
             <div className="navbar bg-primary text-primary-content">
@@ -9,6 +19,12 @@ const Header = () => {
                 <Link className="btn btn-ghost normal-case text-xl" to="/">Home</Link>
                 <Link className="btn btn-ghost normal-case text-xl" to="/login">Login</Link>
                 <Link className="btn btn-ghost normal-case text-xl" to="/register">Register</Link>
+                {
+                    user ? <>
+                        <span>{user.email}</span>
+                        <button onClick={handleLogOut} className="btn btn-xs">Sign out</button>
+                    </> : <Link to="/login">Login </Link>
+                }
             </div>
         </div>
     );
--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
@@ -17,10 +17,28 @@ const AuthProviders = ({children}) => {
         return signInWithEmailAndPassword(auth, email, password);
     }
 
+    const logOut = () =>{
+        return signOut(auth);
+    }
+
+    // observe auth state change
+    useEffect( () =>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('auth state change', currentUser);
+            setUser(currentUser);
+        });
+
+        return () =>{
+            unsubscribe();
+        }
+
+    }, [])
+
     const authInfo = {
         user,
         createUser,
-        signIn
+        signIn,
+        logOut
     }
 
     return (
